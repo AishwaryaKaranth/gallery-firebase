983b3f51386ffc8a4003a89487f3e6b427a7fb26
AishwaryaKaranth <aishwaryakaranth27899@gmail.com>
2021-04-17 17:58:42 +0530

    change firebase_config file

--- gallery/src/firebase/firebase_config.js
+++ gallery/src/firebase/firebase_config.js
@@ -3,12 +3,12 @@ import 'firebase/firestore';
 import 'firebase/storage';
 
 var firebaseConfig = {
-    apiKey: "",
+    apiKey: "AIzaSyDsy9l6e5RHCZ4c8xWe9Bf9Dr4puY8Qr90",
     authDomain: "photo-gallery-2101a.firebaseapp.com",
     projectId: "photo-gallery-2101a",
     storageBucket: "photo-gallery-2101a.appspot.com",
     messagingSenderId: "165768576780",
-    appId: ""
+    appId: "1:165768576780:web:c6df3f28677ec3b5a95329"
   };
   // Initialize Firebase
   firebase.initializeApp(firebaseConfig);
