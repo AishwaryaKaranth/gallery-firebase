d2321dbf7e1532dd4844128a34f814c5489b5422
AishwaryaKaranth <aishwaryakaranth27899@gmail.com>
2021-04-17 18:30:20 +0530

    addded method to add files to storage dynamically

--- gallery/src/customHooks/useStorage.js
+++ gallery/src/customHooks/useStorage.js
@@ -6,4 +6,22 @@ const useStorage=(file)=>{
     const [progress, setProgress]=useState(0);
     const [error, setError]=useState(null);
     const [url, setUrl]=useState(null);
+
+
+useEffect(()=>{
+    const storageRef=proejctStorage.ref(file.name)//this references a file inside default firebase bucket.
+    storageRef.put(file).on('state_changed',(snap)=>{
+        let percentage=(snap.bytesTransferred/snap.totalBytes)*100;
+        setProgress(percentage)
+    },(err)=>{
+        setError(err)
+    },async()=>{
+        const url=await storageRef.getDownloadURL();
+        setUrl(url);
+    })//state change happens multiple times during upload
+},[file]) //the fxn inside useEffect fires every time the dependency array changes.
+return {progress, url, error}
 }
+
+export default useStorage;
+
