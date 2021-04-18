aedf20510ddb35191301e977e42d0ce4aef6917c
AishwaryaKaranth <aishwaryakaranth27899@gmail.com>
2021-04-18 11:12:54 +0530

    added image grid

--- gallery/src/App.js
+++ gallery/src/App.js
@@ -1,10 +1,12 @@
 import React from "react";
 import './App.css';
 import UploadForm from "./components/formUpload.js";
+import ImageGrid from "./components/imageGrid";
 function App() {
   return (
     <div className="App">
       <UploadForm/>
+      <ImageGrid/>
     </div>
   );
 }
