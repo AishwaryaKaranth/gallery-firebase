7ff33ef7f8d0e44751338ebd8cda948367f72c88
AishwaryaKaranth <aishwaryakaranth27899@gmail.com>
2021-04-18 10:55:35 +0530

    hide progress bar using useEffect hook

--- gallery/src/components/ProgressBar.js
+++ gallery/src/components/ProgressBar.js
@@ -1,13 +1,16 @@
-import React from "react";
+import React, { useEffect } from "react";
 import useStorage from '../customHooks/useStorage';
 
 const ProgressBar=({file, setFile})=>{ //destructuring the props
     const {url, progress}=useStorage(file); //this fires the useEffect hook and create a reference
-    console.log(progress,url);
+    console.log(progress, url);
+    useEffect(()=>{
+        if(url){
+            setFile(null);//don't show the progress bar after completion of upload
+        }
+    }, [url, setFile]);// useEffect here does not show the progress bar once the image is upoaded.
     return(
-        <div className="progress-bar">
-            progress
-        </div>
+        <div className="progress-bar" style={{width:progress + '%'}}></div>
     )
 }
 
