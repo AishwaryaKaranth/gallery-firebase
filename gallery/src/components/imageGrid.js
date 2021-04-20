feed5c9c59c7bb6f1b2697346140b3766ebfed31
AishwaryaKaranth <aishwaryakaranth27899@gmail.com>
2021-04-20 18:19:03 +0530

    changes in image grid

--- gallery/src/components/imageGrid.js
+++ gallery/src/components/imageGrid.js
@@ -1,13 +1,22 @@
 import React from "react";
 import useFirestore from "../customHooks/useFirestore";
+import {motion} from "framer-motion";
+
 const ImageGrid = ({setSelect})=>{
     const {docs}=useFirestore('images');//'images' is the name of the collection
     return(
         <div className="img-grid">
             {docs && docs.map(doc=>(
-                <div className="imag-wrap" key={doc.id} onClick={()=>setSelect(doc.url)}>
-                    <img src={doc.url} />
-                </div>
+                <motion.div className="imag-wrap" key={doc.id} 
+                layout
+                whileHover={{opacity:1}}
+                onClick={()=>setSelect(doc.url)}>
+                    <motion.img src={doc.url} 
+                        initial={{opacity:0}}
+                        animate={{opacity:1}}
+                        transition={{delay:1}}
+                    />
+                </motion.div>
             ))}
         </div>
     )
